e17397fb2733035d2a2480394df1c36eb00ccfc6
Noel Dasco Jr <119291271+v88-noel@users.noreply.github.com>
2022-12-21 13:22:19 +0800

    done simple redux toolkit practice

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import './App.css';
-import {useState} from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { decrement, increment, incrementByAmount } from './redux/counter';
 
 function App() {
-  const [count, setCount] = useState(0);
+  const { count } = useSelector((state)=> state.counter)
+  const dispatch = useDispatch();
 
   return (
     <div className="App">
          <h1> The count is: {count}</h1>
-         <button onClick={()=> setCount(count + 1)}>increment</button>
-         <button onClick={()=> setCount(count - 1)}>decrement</button>
+         <button onClick={()=> dispatch(increment())}>increment</button>
+         <button onClick={()=> dispatch(decrement())}>decrement</button>
+         <button onClick={()=> dispatch(incrementByAmount(33))}>Increment by 33</button>
     </div>
   );
 }
